5cf6c85b8556945a579f3663afda258432b6ec13
Slava <Slava9162004@yandex.ru>
2023-01-22 19:39:31 +0300

    1

---
+++ Task36/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+const int size = 6;
+int[] array = new int[size];
+
+for (int i = 0; i < size; i++)
+{
+    array[i] = new Random().Next(0,21);
+}
+Console.WriteLine(string.Join(", ", array));
+int result = 0;
+
+for (int i = 0; i < size; i++)
+{
+    if (i % 2 == 1)
+    {
+        result += array[i];
+    }
+}
+Console.WriteLine(result);
---
+++ Task38/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 38: Задайте массив натуральных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+int[] array = {3,7,22,2,78};
+int max = array[0];
+int min = array[0];
+
+for (int i = 0; i < array.Length; i++)
+{
+    while(array[i] > max)
+    {
+        max = array[i];
+    }
+     while(array[i] < min)
+    {
+        min = array[i];
+    }
+}
+
+Console.WriteLine(string.Join(", ", array));
+int result = max - min;
+Console.WriteLine(result);
